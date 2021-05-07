3e1844bb8e88928a1b2f87b299f37dce72527037
Jesus Carpintero <jesus.carpintero@adevinta.com>
2021-05-07 10:44:54 +0200

    Upgrade flyway

--- db/flyway-clean-schema.sh
+++ db/flyway-clean-schema.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker run --net="host" -v $(pwd):/scripts boxfuse/flyway -user=vulcanito      -password=vulcanito      -url=jdbc:postgresql://localhost:5432/vulcanito      -baselineOnMigrate=true -locations=filesystem:/scripts/ clean
-docker run --net="host" -v $(pwd):/scripts boxfuse/flyway -user=vulcanito_test -password=vulcanito_test -url=jdbc:postgresql://localhost:5432/vulcanito_test -baselineOnMigrate=true -locations=filesystem:/scripts/ clean
+docker run --net=host --rm -v $(pwd):/flyway/sql flyway/flyway:${FLYWAY_VERSION:-7} -user=vulcanito -password=vulcanito -url=jdbc:postgresql://localhost:5432/vulcanito -baselineOnMigrate=true clean
+docker run --net=host --rm -v $(pwd):/flyway/sql flyway/flyway:${FLYWAY_VERSION:-7} -user=vulcanito_test -password=vulcanito_test -url=jdbc:postgresql://localhost:5432/vulcanito_test -baselineOnMigrate=true clean
 #docker exec vulcanito_db psql -c "DROP USER vulcanito_test;" -U postgres
--- db/flyway-migrate.sh
+++ db/flyway-migrate.sh
@@ -4,5 +4,6 @@ docker exec vulcanito_db psql -c "DROP USER IF EXISTS vulcanito_test;" -U postgr
 docker exec vulcanito_db psql -c "CREATE USER vulcanito_test WITH PASSWORD 'vulcanito_test';" -U postgres
 docker exec vulcanito_db psql -c "ALTER USER vulcanito_test WITH SUPERUSER;" -U postgres
 docker exec vulcanito_db psql -c "CREATE DATABASE vulcanito_test;" -U postgres
-docker run --net="host" -v $(pwd):/scripts boxfuse/flyway -user=vulcanito      -password=vulcanito      -url=jdbc:postgresql://localhost:5432/vulcanito      -baselineOnMigrate=true -locations=filesystem:/scripts/sql migrate
-docker run --net="host" -v $(pwd):/scripts boxfuse/flyway -user=vulcanito_test -password=vulcanito_test -url=jdbc:postgresql://localhost:5432/vulcanito_test -baselineOnMigrate=true -locations=filesystem:/scripts/sql,filesystem:/scripts/test-sql migrate
+
+docker run --net=host --rm -v $(pwd):/flyway/sql flyway/flyway:${FLYWAY_VERSION:-7} -user=vulcanito -password=vulcanito -url=jdbc:postgresql://localhost:5432/vulcanito -baselineOnMigrate=true migrate
+docker run --net=host --rm -v $(pwd):/scripts flyway/flyway:${FLYWAY_VERSION:-7} -user=vulcanito_test -password=vulcanito_test -url=jdbc:postgresql://localhost:5432/vulcanito_test -locations=filesystem:/scripts/sql,filesystem:/scripts/test-sql -baselineOnMigrate=true migrate
