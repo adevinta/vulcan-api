fdd0007b7c7e1bbb4adc62d5a657248deecd2d46
Jesus Carpintero <jesus.carpintero@adevinta.com>
2021-10-21 11:15:42 +0200

    Upgrade pg

--- db/flyway-clean-schema.sh
+++ db/flyway-clean-schema.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-docker run --net="host" -v $(pwd):/scripts boxfuse/flyway -user=vulcanito      -password=vulcanito      -url=jdbc:postgresql://localhost:5432/vulcanito      -baselineOnMigrate=true -locations=filesystem:/scripts/ clean
-docker run --net="host" -v $(pwd):/scripts boxfuse/flyway -user=vulcanito_test -password=vulcanito_test -url=jdbc:postgresql://localhost:5432/vulcanito_test -baselineOnMigrate=true -locations=filesystem:/scripts/ clean
+
+docker run --net=host --rm -v "$PWD":/flyway/sql flyway/flyway:"${FLYWAY_VERSION:-8}" \
+    -user=vulcanito -password=vulcanito -url=jdbc:postgresql://localhost:5432/vulcanito -baselineOnMigrate=true clean
+
+docker run --net=host --rm -v "$PWD":/flyway/sql flyway/flyway:"${FLYWAY_VERSION:-8}" \
+    -user=vulcanito_test -password=vulcanito_test -url=jdbc:postgresql://localhost:5432/vulcanito_test -baselineOnMigrate=true clean
+
 #docker exec vulcanito_db psql -c "DROP USER vulcanito_test;" -U postgres
--- db/setup_test_db.sh
+++
@@ -1,6 +0,0 @@
-psql -h localhost -c "CREATE DATABASE vulcanito_test;" -U vulcan vulcan
-psql -h localhost -c "CREATE USER vulcanito_test WITH PASSWORD 'vulcanito_test';" -U vulcan vulcan
-psql -h localhost -c "ALTER USER vulcanito_test WITH SUPERUSER;" -U vulcan vulcan
-db/flyway/flyway -user=vulcanito_test -password=vulcanito_test -url=jdbc:postgresql://localhost:5432/vulcanito_test -baselineOnMigrate=true -locations=filesystem:db/sql,filesystem:db/test-sql migrate
-psql -h localhost -c "CREATE DATABASE vulcanito WITH TEMPLATE vulcanito_test OWNER vulcanito_test;" -U vulcan vulcan
-
