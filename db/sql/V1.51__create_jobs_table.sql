f07bb6322730e7a933748950f01b7ba1a6c7326c
Julian Vilas <julian.vilas@gmail.com>
2021-12-10 12:45:24 +0100

    Add migration to create jobs table

---
+++ db/sql/V1.51__create_jobs_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE jobs (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	team_id UUID,
+    operation TEXT NOT NULL,
+    status TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE,
+
+    CONSTRAINT fk_team_id FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE
+);
