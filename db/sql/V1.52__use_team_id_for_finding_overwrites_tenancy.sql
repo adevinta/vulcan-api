3f460c037fd6373be667c73bce1f27bcc656678d
Gerard Guillemas Martos <gerard@guillemas.com>
2022-08-17 11:04:49 +0200

    Use team_id for overwrite tenancy but keep tags

--- db/sql/V1.52__use_team_id_for_finding_overwrites_tenancy.sql
+++ db/sql/V1.52__use_team_id_for_finding_overwrites_tenancy.sql
@@ -1,2 +1,3 @@
 ALTER TABLE finding_overwrites ADD COLUMN team_id UUID REFERENCES teams (id) ON DELETE CASCADE;
 UPDATE finding_overwrites SET team_id = teams.id FROM teams WHERE teams.tag = finding_overwrites.tag;
+ALTER TABLE finding_overwrites ALTER COLUMN tag DROP NOT NULL;
