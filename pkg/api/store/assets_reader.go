33f4b2f15597918dd51c46bb7cc11562066fd9d7
Manel Montilla <manel.montilla@gmail.com>
2022-08-24 16:40:46 +0200

    Clean AssetsReader code

--- pkg/api/store/assets_reader.go
+++ pkg/api/store/assets_reader.go
@@ -17,34 +17,8 @@ import (
 // when there are no more assets to read.
 var ErrReadAssetsFinished = goerrors.New("no more assets")
 
-// ReadAssetsToken defines the opaque structure that keeps the required state
-// between calls to the methods StartReadingAssets, ReadAssets and
-// FinishReadingAssets.
-type ReadAssetsToken struct {
-	next     string
-	pageSize int
-	tx       *gorm.DB
-}
-
-// ReadAssetsResult contains the data returned by the ReadNextAssetsPage method
-// of the vulcanitoStore.
-type ReadAssetsResult struct {
-	Assets []*api.Asset
-	Token  ReadAssetsToken
-	More   bool
-}
-
-// StartReadingAssetsResult contains the data returned by the
-// StartReadingAssetsResult of the vulcanitoStore.
-type StartReadingAssetsResult struct {
-	Assets     []*api.Asset
-	Token      ReadAssetsToken
-	More       bool
-	TotalPages int
-}
-
-// NewAssetReader creates a new AssetReader with the given page size.
-// If the lock param is set to true it will lock for writing the tables:
+// NewAssetReader creates a new AssetReader with the given page size. If the
+// lock param is set to true it will lock for writing the following tables:
 // Assets, Teams and AssetAnnotations.
 func (db vulcanitoStore) NewAssetReader(lock bool, pageSize int) (AssetsReader, error) {
 	if pageSize < 1 {
@@ -56,7 +30,7 @@ func (db vulcanitoStore) NewAssetReader(lock bool, pageSize int) (AssetsReader, 
 		return AssetsReader{}, db.logError(errors.Database(tx.Error))
 	}
 	// Even though, theoretically, between calls to the Read method it could
-	// pass enough time for the transaction to be timed out by postgres
+	// pass enough time for the transaction to be timed out by Postgres
 	// depending on the configured max amount of time for a transaction to be
 	// idle, it's in practice not very likely to happen, so by now, we are not
 	// implementing a `NOP loop` to ensure the transaction is not closed for
@@ -90,11 +64,9 @@ func (db vulcanitoStore) NewAssetReader(lock bool, pageSize int) (AssetsReader, 
 	return reader, nil
 }
 
-// AssetsReader allows to read all the assets stored in Vulcan using pages with
-// user defined the size.
+// AssetsReader reads all the assets stored in Vulcan using pages with
+// a configurable size.
 type AssetsReader struct {
-	// The total number of assets that the reader will read.
-	Total    int
 	next     string
 	pageSize int
 	tx       *gorm.DB
@@ -103,8 +75,9 @@ type AssetsReader struct {
 	lock     bool
 }
 
-// Read returns the next page of the assets, according to the the page size the
-// reader was created with and the total number of assets to read.
+// Read returns the next page of the assets according to the page size of the
+// [AssetReader]. When there are no more assets to read it will return the the
+// error [ErrReadAssetsFinished].
 func (a *AssetsReader) Read() ([]*api.Asset, error) {
 	if !a.more {
 		return nil, ErrReadAssetsFinished
@@ -113,13 +86,12 @@ func (a *AssetsReader) Read() ([]*api.Asset, error) {
 	if a.next == "" {
 		return a.readFirst()
 	}
-	// stm := `SELECT * FROM assets a JOIN teams t ON a.team_id = t.id JOIN asset_annotations an ON
-	// an.asset_id = a.id where a.id >= ? ORDER BY a.id LIMIT ?`
+
 	assets := make([]*api.Asset, 0)
 	limit := a.pageSize + 1
 	next := a.next
 	tx := a.tx
-	//res := tx.Raw(stm, next, limit).Scan(&assets)
+
 	res := tx.Preload("Team").
 		Preload("AssetType").
 		Preload("AssetAnnotations").
@@ -148,20 +120,19 @@ func (a *AssetsReader) Read() ([]*api.Asset, error) {
 	return assets, nil
 }
 
-// Close closes the reader, if the reader was created with the lock parameter
-// set to true, it unlocks the tables that were locked when it was created.
+// Close closes the reader and unlocks the tables that were locked when it was
+// created.
 func (a *AssetsReader) Close() error {
+	// Notice the tables are automatically unlocked when the transaction is committed.
 	return a.tx.Commit().Error
 }
 
 func (a *AssetsReader) readFirst() ([]*api.Asset, error) {
 	tx := a.tx
-	// stm := `SELECT * FROM assets JOIN teams ON assets.team_id = teams.id JOIN asset_annotations ON
-	// asset_annotations.asset_id = assets.id ORDER BY assets.id LIMIT ?`
 	assets := make([]*api.Asset, 0)
 	pageSize := a.pageSize
 	limit := pageSize + 1
-	//res := tx.Raw(stm, limit).Scan(&assets)
+
 	res := tx.Preload("Team").
 		Preload("AssetType").
 		Preload("AssetAnnotations").
