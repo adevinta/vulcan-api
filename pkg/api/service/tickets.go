65411cd17e2122563b87e52b511b9ab4aed1339e
Jesús Carpintero <jesus.carpintero@gmail.com>
2024-04-11 13:05:07 +0200

    Allow tracker integration for admins/observers

--- pkg/api/service/tickets.go
+++ pkg/api/service/tickets.go
@@ -6,19 +6,28 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/adevinta/vulcan-api/pkg/api"
 )
 
+func isTrackerAllowed(ctx context.Context) bool {
+	user, err := api.UserFromContext(ctx)
+	return err == nil && (user.Admin != nil && *user.Admin) || (user.Observer != nil && *user.Observer)
+}
+
 // CreateFindingTicket requests the creation of a ticket in the ticket tracker
 // with the values stored in the argument ticket.
 func (s vulcanitoService) CreateFindingTicket(ctx context.Context, ticket api.FindingTicketCreate) (*api.Ticket, error) {
+	if !isTrackerAllowed(ctx) {
+		return nil, fmt.Errorf("unauthorized to create a ticket")
+	}
 	return s.vulcantrackerClient.CreateTicket(ctx, ticket)
 }
 
 // GetFindingTicket makes a request to vulcan tracker to find a ticket for a team.
 func (s vulcanitoService) GetFindingTicket(ctx context.Context, findingID, teamID string) (*api.Ticket, error) {
-	if s.vulcantrackerClient == nil {
+	if s.vulcantrackerClient == nil || !isTrackerAllowed(ctx) {
 		return nil, nil
 	}
 	return s.vulcantrackerClient.GetFindingTicket(ctx, findingID, teamID)
@@ -27,6 +36,9 @@ func (s vulcanitoService) GetFindingTicket(ctx context.Context, findingID, teamI
 // IsATeamOnboardedInVulcanTracker return if a team is onboarded in vulcan tracker.
 func (s vulcanitoService) IsATeamOnboardedInVulcanTracker(ctx context.Context, teamID string, onboardedTeams []string) bool {
 	if s.vulcantrackerClient == nil {
+		return false
+	}
+	if !isTrackerAllowed(ctx) {
 		return false
 	}
 	for _, team := range onboardedTeams {
--- pkg/api/service/tickets_test.go
+++ pkg/api/service/tickets_test.go
@@ -8,22 +8,90 @@ import (
 	"context"
 	"testing"
 
+	"github.com/adevinta/vulcan-api/pkg/api"
+	"github.com/adevinta/vulcan-api/pkg/common"
 	"github.com/adevinta/vulcan-api/pkg/tickets"
 )
 
 func TestVulcanitoService_OnboardedTeam(t *testing.T) {
-	srv := vulcanitoService{}
-	if srv.IsATeamOnboardedInVulcanTracker(context.TODO(), "whatever-team", []string{"whatever-team"}) {
-		t.Fatal("vulcanitoService.IsATeamOnboardedInVulcanTracker() no-client")
+	srv := vulcanitoService{
+		vulcantrackerClient: tickets.NewClient(nil, "", true),
 	}
-	srv.vulcantrackerClient = tickets.NewClient(nil, "", true)
-	if !srv.IsATeamOnboardedInVulcanTracker(context.TODO(), "whatever-team", []string{"*"}) {
-		t.Fatal("vulcanitoService.IsATeamOnboardedInVulcanTracker() all")
+	ctx := context.Background()
+	ctxAdmin := api.ContextWithUser(ctx, api.User{Admin: common.Bool(true)})
+	ctxObserver := api.ContextWithUser(ctx, api.User{Observer: common.Bool(true)})
+
+	tests := []struct {
+		name           string
+		srv            vulcanitoService
+		context        context.Context
+		onboardedTeams []string
+		teamID         string
+		want           bool
+	}{
+		{
+			name:           "OnboardedAllTeamsNoClient",
+			srv:            vulcanitoService{},
+			context:        ctxAdmin,
+			onboardedTeams: []string{"whatever-team", "*"},
+			teamID:         "other-team",
+			want:           false,
+		},
+		{
+			name:           "HappyPath-admin",
+			srv:            srv,
+			context:        ctxAdmin,
+			onboardedTeams: []string{"whatever-team"},
+			teamID:         "whatever-team",
+			want:           true,
+		},
+		{
+			name:           "HappyPath-observer",
+			srv:            srv,
+			context:        ctxObserver,
+			onboardedTeams: []string{"whatever-team"},
+			teamID:         "whatever-team",
+			want:           true,
+		},
+		{
+			name:           "HappyPath-no-auth",
+			srv:            srv,
+			context:        ctx,
+			onboardedTeams: []string{"whatever-team"},
+			teamID:         "whatever-team",
+			want:           false,
+		},
+		{
+			name:           "NoOnboardedTeam",
+			srv:            srv,
+			context:        ctxAdmin,
+			onboardedTeams: []string{"whatever-team"},
+			teamID:         "other-team",
+			want:           false,
+		},
+		{
+			name:           "OnboardedAllTeams",
+			srv:            srv,
+			context:        ctxAdmin,
+			onboardedTeams: []string{"whatever-team", "*"},
+			teamID:         "other-team",
+			want:           true,
+		},
+		{
+			name:           "NoOnboardedAllTeams",
+			srv:            srv,
+			context:        ctxAdmin,
+			onboardedTeams: []string{},
+			teamID:         "other-team",
+			want:           false,
+		},
 	}
-	if !srv.IsATeamOnboardedInVulcanTracker(context.TODO(), "whatever-team", []string{"whatever-team"}) {
-		t.Fatal("vulcanitoService.IsATeamOnboardedInVulcanTracker() explicit")
-	}
-	if srv.IsATeamOnboardedInVulcanTracker(context.TODO(), "whatever-team", []string{"other"}) {
-		t.Fatal("vulcanitoService.IsATeamOnboardedInVulcanTracker() not")
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.srv.IsATeamOnboardedInVulcanTracker(tt.context, tt.teamID, tt.onboardedTeams) != tt.want {
+				t.Fatal("error")
+			}
+		})
 	}
 }
