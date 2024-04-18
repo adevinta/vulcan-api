2074c97e45a24f0478e61da7c0929b26056eb425
Jesús Carpintero <jesus.carpintero@adevinta.com>
2024-04-18 15:49:15 +0200

    Allow configure tracker for all the teams (#339)
    * Allow configure tracker for all the teams
    * Allow tracker integration for admins/observers

--- pkg/api/service/tickets.go
+++ pkg/api/service/tickets.go
@@ -6,18 +6,28 @@ package service
 
 import (
 	"context"
+	"fmt"
+
 	"github.com/adevinta/vulcan-api/pkg/api"
 )
+
+func isTrackerAllowed(ctx context.Context) bool {
+	user, err := api.UserFromContext(ctx)
+	return err == nil && (user.Admin != nil && *user.Admin) || (user.Observer != nil && *user.Observer)
+}
 
 // CreateFindingTicket requests the creation of a ticket in the ticket tracker
 // with the values stored in the argument ticket.
 func (s vulcanitoService) CreateFindingTicket(ctx context.Context, ticket api.FindingTicketCreate) (*api.Ticket, error) {
+	if !isTrackerAllowed(ctx) {
+		return nil, fmt.Errorf("unauthorized to create a ticket")
+	}
 	return s.vulcantrackerClient.CreateTicket(ctx, ticket)
 }
 
 // GetFindingTicket makes a request to vulcan tracker to find a ticket for a team.
 func (s vulcanitoService) GetFindingTicket(ctx context.Context, findingID, teamID string) (*api.Ticket, error) {
-	if s.vulcantrackerClient == nil {
+	if s.vulcantrackerClient == nil || !isTrackerAllowed(ctx) {
 		return nil, nil
 	}
 	return s.vulcantrackerClient.GetFindingTicket(ctx, findingID, teamID)
@@ -28,8 +38,11 @@ func (s vulcanitoService) IsATeamOnboardedInVulcanTracker(ctx context.Context, t
 	if s.vulcantrackerClient == nil {
 		return false
 	}
+	if !isTrackerAllowed(ctx) {
+		return false
+	}
 	for _, team := range onboardedTeams {
-		if team == teamID {
+		if team == teamID || team == "*" {
 			return true
 		}
 	}
